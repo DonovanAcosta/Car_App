cc49594122e548d32f7d3ef38ccc9b235a0e064d
ethanrodriguez5433 <ethanrodriguez5433@gmail.com>
2024-11-12 16:19:00 -0600

    Seperated car pop up for cleaner code

--- homepage.py
+++ homepage.py
@@ -5,6 +5,7 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 
+from carpopup import CarPopUp
 from classes import Car
 
 class HomePageLayout(FloatLayout):
@@ -64,97 +65,7 @@ class HomePageLayout(FloatLayout):
                 car = db[car_id]
                 car_button_text = car.name if car.name != '' else f'{car.make}: {car.model}'
                 car_button = Button(text=car_button_text, size_hint=(1,0.2), pos_hint={'x':0, 'y':button_count})
-                car_button.bind(on_press=lambda instance, c=car: self.show_car_details(c))
+                car_button.bind(on_press=lambda instance, c=car: CarPopUp(car=c, homepage=self))
                 self.add_widget(car_button)
                 button_count -= 0.2
 
-    def show_car_details(self, car):
-        layout = FloatLayout()
-
-        # Display car details using labels
-        detail_label = Label(
-            text=f"ID: {car.id}\nMake: {car.make}\nModel: {car.model}\nYear: {car.year}\nName: {car.name}",
-            size_hint=(0.8, 0.4), pos_hint={'x': 0.1, 'y': 0.5}
-        )
-        layout.add_widget(detail_label)
-
-        # Add Delete and Edit buttons
-        delete_button = Button(text='Delete Car', size_hint=(0.33, 0.1), pos_hint={'x': 0.0, 'y': 0.0})
-        delete_button.bind(on_press=lambda instance: self.show_delete_confirmation(car))
-        layout.add_widget(delete_button)
-
-        edit_button = Button(text='Edit Car', size_hint=(0.33, 0.1), pos_hint={'x': 0.33, 'y': 0.0})
-        edit_button.bind(on_press=lambda instance: self.show_edit_car_popup(car))
-        layout.add_widget(edit_button)
-
-        view_log_button = Button(text='View Logs', size_hint=(0.33, 0.1), pos_hint={'x':0.66, 'y':0.0})
-        view_log_button.bind(on_press=lambda instance: self.view_car_logs(car))
-        layout.add_widget(view_log_button)
-
-        self.popup = Popup(title=f'{car.name}', content=layout, size_hint=(0.8, 0.8))
-        self.popup.open()
-
-    def show_delete_confirmation(self, car):
-        layout = FloatLayout()
-        label = Label(text="Are you sure you want to delete this car?", size_hint=(0.8, 0.3), pos_hint={'x': 0.1, 'y': 0.5})
-        layout.add_widget(label)
-
-        yes_button = Button(text="Yes", size_hint=(0.4, 0.2), pos_hint={'x': 0.05, 'y': 0.2})
-        no_button = Button(text="No", size_hint=(0.4, 0.2), pos_hint={'x': 0.55, 'y': 0.2})
-
-        layout.add_widget(yes_button)
-        layout.add_widget(no_button)
-
-        confirmation_popup = Popup(title="Confirm Deletion", content=layout, size_hint=(0.6, 0.4))
-
-        yes_button.bind(on_press=lambda instance: self.delete_car(car, confirmation_popup))
-        no_button.bind(on_press=confirmation_popup.dismiss)
-
-        confirmation_popup.open()
-
-    def delete_car(self, car, confirmation_popup):
-        with shelve.open('car_database') as db:
-            del db[car.id]
-            print(f'Car {car.name} with ID {car.id} deleted.')
-
-        confirmation_popup.dismiss()
-        self.popup.dismiss()
-        self.display_home_page()
-
-    def view_car_logs(self, car):
-        return
-
-
-    def show_edit_car_popup(self, car):
-        layout = FloatLayout()
-
-        self.make_input = TextInput(text=car.make, size_hint=(0.8, 0.1), pos_hint={'x': 0.1, 'y': 0.7})
-        self.model_input = TextInput(text=car.model, size_hint=(0.8, 0.1), pos_hint={'x': 0.1, 'y': 0.55})
-        self.year_input = TextInput(text=car.year, size_hint=(0.8, 0.1), pos_hint={'x': 0.1, 'y': 0.4})
-        self.name_input = TextInput(text=car.name, size_hint=(0.8, 0.1), pos_hint={'x': 0.1, 'y': 0.25})
-
-        submit_button = Button(text='Save Changes', size_hint=(0.5, 0.1), pos_hint={'x': 0.25, 'y': 0.1})
-        submit_button.bind(on_press=lambda instance: self.update_car_in_db(car))
-
-        layout.add_widget(self.make_input)
-        layout.add_widget(self.model_input)
-        layout.add_widget(self.year_input)
-        layout.add_widget(self.name_input)
-        layout.add_widget(submit_button)
-
-        self.edit_popup = Popup(title='Edit Car', content=layout, size_hint=(0.8, 0.8))
-        self.edit_popup.open()
-
-    def update_car_in_db(self, car):
-        car.make = self.make_input.text
-        car.model = self.model_input.text
-        car.year = self.year_input.text
-        car.name = self.name_input.text
-
-        with shelve.open('car_database') as db:
-            db[car.id] = car
-            print(f'Car {car.name} with ID {car.id} updated.')
-
-        self.edit_popup.dismiss()
-        self.popup.dismiss()
-        self.display_home_page()
