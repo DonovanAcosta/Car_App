d92eb4cec556d550d412ddc50e916a6db95d3fff
DonovanAcosta <donovan25@outlook.com>
2024-12-09 13:34:13 -0600

    Added color and validation check

--- logpage.py
+++ logpage.py
@@ -7,6 +7,7 @@ from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import Screen
+from kivy.graphics import Color, Rectangle
 
 from classes import Maintenance, Mod
 
@@ -14,6 +15,14 @@ class LogPage(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.car = None
+        self.bind(size=self.update_background, pos=self.update_background)
+        with self.canvas.before:
+            self.bg_color = Color(0.05, 0.1, 0.2, 1)
+            self.bg_rect = Rectangle(size=self.size, pos=self.pos)
+    
+    def update_background(self, *args):
+        self.bg_rect.size = self.size
+        self.bg_rect.pos = self.pos
         
         
 
@@ -22,17 +31,18 @@ class LogPage(Screen):
         self.clear_widgets()
 
         ###Back Button
-        back_button = Button(text='<-', size_hint=(0.33,0.1), pos_hint={'x':0.0,'y':0.0})
+        back_button = Button(text='<-', size_hint=(0.33,0.1), pos_hint={'x':0.0,'y':0.0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
+
         back_button.bind(on_press=self.backward)
         self.add_widget(back_button)
 
         ####Add Maintenance Button
-        add_mait_button = Button(text='Add Maintenance', size_hint=(0.33, 0.1), pos_hint={'x':0.33,'y':0})
+        add_mait_button = Button(text='Add Maintenance', size_hint=(0.33, 0.1), pos_hint={'x':0.33,'y':0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
         add_mait_button.bind(on_press=self.show_add_mait_popup)
         self.add_widget(add_mait_button)
 
         ###Add Mod Button
-        add_mod_button = Button(text='Add Mod', size_hint=(0.33,0.1), pos_hint={'x':0.66,'y':0.0})
+        add_mod_button = Button(text='Add Mod', size_hint=(0.34,0.1), pos_hint={'x':0.66,'y':0.0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
         add_mod_button.bind(on_press=self.show_add_mod_popup)
         self.add_widget(add_mod_button)
 
@@ -42,7 +52,7 @@ class LogPage(Screen):
 
         log_y = 0.9
         for log in logs:
-            log_label = Button(text=self.format_log_entry(log), size_hint=(1,0.1),pos_hint={'x':0.0,'y':log_y})
+            log_label = Button(text=self.format_log_entry(log), size_hint=(1,0.1),pos_hint={'x':0.0,'y':log_y},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
             log_label.bind(on_press=lambda instance, log=log: self.show_log_popup(instance, log))
             self.add_widget(log_label)
             log_y-=0.1
@@ -57,30 +67,31 @@ class LogPage(Screen):
         #############################
         ###Input for maintenance info
         #############################
-        self.name_input = TextInput(hint_text='Maintenance Name', size_hint=INPUT_SIZE, pos_hint={'x':0.1, 'y':0.725})
-        self.date_input = TextInput(hint_text="Date (YYYY-MM-DD)", size_hint=INPUT_SIZE, pos_hint={'x': 0.1, 'y': 0.575})
-        self.freq_input = TextInput(hint_text='Frequency', size_hint=(0.3,0.1), pos_hint={'x':0.1, 'y':0.425})
+        self.name_input = TextInput(hint_text='Maintenance Name', size_hint=INPUT_SIZE, pos_hint={'x':0.1, 'y':0.725}, background_color = (1,1,1, 0.7))
+        self.date_input = TextInput(hint_text="Date (YYYY-MM-DD)", size_hint=INPUT_SIZE, pos_hint={'x': 0.1, 'y': 0.575}, background_color = (1,1,1, 0.7))
+        self.freq_input = TextInput(hint_text='Frequency', size_hint=(0.3,0.1), pos_hint={'x':0.1, 'y':0.425}, background_color = (1,1,1, 0.7))
         
         ###Drop down box
         dropdown = DropDown()
         for unit in ['Days','Months', "Miles"]:
-            btn = Button(text=unit, size_hint_y=None, height = 44)
+            btn = Button(text=unit, size_hint_y=None, height = 44,background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
             btn.bind(on_release=lambda btn: dropdown.select(btn.text))
             dropdown.add_widget(btn)
 
 
-        self.unit_button = Button(text='Select Unit', size_hint=(0.4,0.1), pos_hint={'x':0.5,'y':0.425})
+
+        self.unit_button = Button(text='Select Unit', size_hint=(0.4,0.1), pos_hint={'x':0.5,'y':0.425},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
         self.unit_button.bind(on_release=dropdown.open)
         dropdown.bind(on_select=lambda instance, x:setattr(self.unit_button, 'text',x))
         layout.add_widget(self.unit_button)
 
-
-        self.descr_input = TextInput(hint_text='Description', size_hint=(0.8, 0.3), pos_hint={'x':0.1, 'y':0.125})
+        self.descr_input = TextInput(hint_text='Description', size_hint=(0.8, 0.3), pos_hint={'x':0.1, 'y':0.125}, background_color = (1,1,1, 0.7))
 
         ###Submit and Cancel buttons
-        submit_button = Button(text='Submit', size_hint=(0.4,0.1), pos_hint={'x':0.1, 'y':0.0})
+        submit_button = Button(text='Submit', size_hint=(0.4,0.1), pos_hint={'x':0.1, 'y':0.0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
         submit_button.bind(on_press=self.add_maintenance)
-        cancel_button = Button(text='Cancel', size_hint=(0.4,0.1), pos_hint={'x':0.5, 'y':0.0})
+        cancel_button = Button(text='Cancel', size_hint=(0.4,0.1), pos_hint={'x':0.5, 'y':0.0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
+
         cancel_button.bind(on_press=self.close_popup)
 
         ###Add widgets
@@ -92,7 +103,8 @@ class LogPage(Screen):
         layout.add_widget(cancel_button)
 
         ###Create Popup
-        self.popup = Popup(title='Add Maintenance', content=layout, size_hint=(0.8, 0.8))
+        self.popup = Popup(title='Add Maintenance', content=layout, size_hint=(0.8, 0.8), background='')
+        self.popup.background_color = (0.05, 0.1, 0.2, 1)
         self.popup.open()
     
     def add_maintenance(self, instance):
@@ -118,16 +130,18 @@ class LogPage(Screen):
         #############################
         ###Input for maintenance info
         #############################
-        self.name_input = TextInput(hint_text='Mod Name', size_hint=INPUT_SIZE, pos_hint={'x':0.1, 'y':0.725})
-        self.date_input = TextInput(hint_text="Date (YYYY-MM-DD)", size_hint=INPUT_SIZE, pos_hint={'x': 0.1, 'y': 0.575})
-        self.part_input = TextInput(hint_text='Part Name', size_hint=(0.3,0.1), pos_hint={'x':0.1, 'y':0.425})
-        self.mech_input = TextInput(hint_text='Mechanic', size_hint=(0.4,0.1), pos_hint={'x':0.5,'y':0.425})
-        self.descr_input = TextInput(hint_text='Description', size_hint=(0.8, 0.3), pos_hint={'x':0.1, 'y':0.125})
+
+        self.name_input = TextInput(hint_text='Mod Name', size_hint=INPUT_SIZE, pos_hint={'x':0.1, 'y':0.725}, background_color = (1,1,1, 0.7))
+        self.date_input = TextInput(hint_text="Date (YYYY-MM-DD)", size_hint=INPUT_SIZE, pos_hint={'x': 0.1, 'y': 0.575}, background_color = (1,1,1, 0.7))
+        self.part_input = TextInput(hint_text='Part Name', size_hint=(0.3,0.1), pos_hint={'x':0.1, 'y':0.425}, background_color = (1,1,1, 0.7))
+        self.mech_input = TextInput(hint_text='Mechanic', size_hint=(0.4,0.1), pos_hint={'x':0.5,'y':0.425}, background_color = (1,1,1, 0.7))
+        self.descr_input = TextInput(hint_text='Description', size_hint=(0.8, 0.3), pos_hint={'x':0.1, 'y':0.125}, background_color = (1,1,1, 0.7))
 
         ###Submit and Cancel buttons
-        submit_button = Button(text='Submit', size_hint=(0.4,0.1), pos_hint={'x':0.1, 'y':0.0})
+        submit_button = Button(text='Submit', size_hint=(0.4,0.1), pos_hint={'x':0.1, 'y':0.0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
         submit_button.bind(on_press=self.add_mod)
-        cancel_button = Button(text='Cancel', size_hint=(0.4,0.1), pos_hint={'x':0.5, 'y':0.0})
+        cancel_button = Button(text='Cancel', size_hint=(0.4,0.1), pos_hint={'x':0.5, 'y':0.0},background_color=(0, 0.6, 0.8, 1),color=(1, 1, 1, 1))
+
         cancel_button.bind(on_press=self.close_popup)
 
         ###Add widgets
@@ -140,7 +154,8 @@ class LogPage(Screen):
         layout.add_widget(cancel_button)
 
         ###Create Popup
-        self.popup = Popup(title='Add Mod', content=layout, size_hint=(0.8, 0.8))
+        self.popup = Popup(title='Add Mod', content=layout, size_hint=(0.8, 0.8), background='')
+        self.popup.background_color = (0.05, 0.1, 0.2, 1)
         self.popup.open()
 
     def add_mod(self, instance):
@@ -212,7 +227,8 @@ class LogPage(Screen):
         label = Label(text=f'Date:{log.date}\n')
         layout.add_widget(label)
 
-        self.popup = Popup(title=f'{log.name}', content=layout, size_hint=(0.8, 0.8))
+        self.popup = Popup(title=f'{log.name}', content=layout, size_hint=(0.8, 0.8), background='')
+        self.popup.background_color = (0.05, 0.1, 0.2, 1)
         self.popup.open()
 
         return
